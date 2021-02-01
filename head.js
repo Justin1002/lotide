a83034e39c12397e5fa5ef2f3115acd53218b431
Justin Ly <jly3@ualberta.ca>
2021-02-01 21:06:13 +0000

    added head function

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑️🤑️🤑️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡️😡️😡️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const head = function(arr) {
+  return arr[0]
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+
