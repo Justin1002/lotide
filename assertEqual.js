8debf2742aa67fbf89999aa7929f866f133deb72
Justin Ly <jly3@ualberta.ca>
2021-02-01 20:59:01 +0000

    Added assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤑️🤑️🤑️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡️😡️😡️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("LHL", "LHL");
+assertEqual(1.2, 1);
