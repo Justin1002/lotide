8affb85efb2d7f98e99afbd1e4a220ef713ea955
Justin Ly <jly3@ualberta.ca>
2021-02-02 21:08:54 +0000

    added flatten function

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2) === true) {
+    console.log(`🤑️🤑️🤑️ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+  else {
+    console.log(`😡️😡️😡️ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const flatten = function(arr) {
+  let newArr = []
+  for (const elem of arr) {
+    if (Array.isArray(elem)) {
+      for (j = 0; j < elem.length; j++) {
+        newArr.push(elem[j])
+      }
+    }
+    else {
+      newArr.push(elem)
+    }
+  }
+  return newArr
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]))
+
+assertArraysEqual(flatten([1,[2,3,4],5]),[1,2,3,4,5])
