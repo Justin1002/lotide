57138e2df52b768e3023ab2777bd4347557de723
Justin Ly <jly3@ualberta.ca>
2021-02-08 17:43:58 +0000

    refactored tail with module exports

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🤑️🤑️🤑️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡️😡️😡️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let modArr = [];
   for (let i = 1; i < arr.length; i++) {
@@ -14,14 +6,4 @@ const tail = function(arr) {
   return modArr;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const test = tail([1]);
-assertEqual(test.length, 0);
+module.exports = tail
---
+++ test/tailTest.js
@@ -0,0 +1,14 @@
+assertEqual = require('../assertEqual')
+tail = require('../tail')
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const test = tail([1]);
+assertEqual(test.length, 0);
