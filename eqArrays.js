bf7c428be4f0ecc6695c188d2a8acc31613ecc2a
Justin Ly <jly3@ualberta.ca>
2021-02-08 17:48:25 +0000

    refactored eqArrays with module exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🤑️🤑️🤑️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡️😡️😡️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -27,13 +18,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-//console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-//console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-
-//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-assertEqual(eqArrays([[1,2,3], 2, 3], [[1,2,3], 2, 3]), true);
-assertEqual(eqArrays([[[1,2,3],2,3], 2, 3], [[[1,2],2,3], 2, 3]), false);
+module.exports = eqArrays
