a6836533546396f9dd62ac5ad4e48edca0549c5f
Justin Ly <jly3@ualberta.ca>
2021-02-08 18:17:39 +0000

    added mocha and chai testing functions to middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,17 @@
-assertArraysEqual = require('../assertArraysEqual')
 middle = require('../middle')
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,3,3,4]), [3,3]);
+describe("#middle", () => {
+  it ("returns an empty array when a single array is input", () => {
+    assert.deepEqual(middle([1]),[]);
+  });
+
+  it ("returns [2] when we input [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+
+  it ("returns [3,3] when we input [1,3,3,4]", () => {
+    assert.deepEqual(middle([1,3,3,4]),[3,3]);
+  });
+
+});
